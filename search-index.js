1c3b0777128005e86c5494af46820bcfb54d121c
rzmk <rzmk@users.noreply.github.com>
2023-09-19 04:11:52 +0000

    deploy: 4ef3cf0da2478ce9e510114936f3d42d69f2c503

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"ladderz":{"doc":"Non-production implementations of mathematical and …","t":"AAF","n":["pre_algebra","unit1","get_factor_pairs"],"q":[[0,"ladderz"],[1,"ladderz::pre_algebra"],[2,"ladderz::pre_algebra::unit1"]],"d":["Various pre-algebra implementations including multiples …","Factors and multiples","Generates a <code>HashSet</code> of factor pairs for a given positive …"],"i":[0,0,0],"f":[0,0,[1,2]],"c":[],"p":[[15,"u32"],[3,"HashSet"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"ladderz":["",[["pre_algebra",[],["unit1.rs"]]],["lib.rs","pre_algebra.rs"]]\
+}');
+createSourceSidebar();
