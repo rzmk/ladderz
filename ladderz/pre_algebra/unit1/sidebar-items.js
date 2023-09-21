48e3274c71bf34db920fc74f4f94e39d48165bc8
rzmk <rzmk@users.noreply.github.com>
2023-09-21 04:36:12 +0000

    deploy: aefec90457917cfa2a9ab9c3778e04d76d318b89

--- ladderz/pre_algebra/unit1/sidebar-items.js
+++ ladderz/pre_algebra/unit1/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["get_factor_pairs","get_factors","is_factor","is_multiple"]};
+window.SIDEBAR_ITEMS = {"fn":["get_factor_pairs","get_factors","get_multiples_in_range","is_factor","is_multiple"]};
